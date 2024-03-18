e749bb70484ee496ead89ad2955601ee70ff1bad
yousunning <151820436+yousunning@users.noreply.github.com>
2024-03-18 22:37:09 +0900

    Delete chapter03 directory

--- chapter03/src/연습/p36.java
+++
@@ -1,8 +0,0 @@
-package 연습;
-
-public class p36 {
- public static void main (String [] args) {
-	 //화면에 문장을 출력함
-	 System.out.println("자바프로그래밍 재미따!");
- }
-}
--- chapter03/src/연습/p45.java
+++
@@ -1,9 +0,0 @@
-package 연습;
-// 내 나이 넣어 만들어보기
-public class p45 {
-	public static void main (String[] args) {
-		int level ;
-		level = 25;
-		System.out.println(level);
-	}
-}
